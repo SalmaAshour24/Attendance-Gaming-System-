bbaaee345bfc3f97c49ce9f96abb1b5f6789375e
MahaDahshan <mahadahshan@gmail.com>
2021-12-15 22:48:34 +0200

    Update students_points.dart

--- lib/screens/doctor/students_points.dart
+++ lib/screens/doctor/students_points.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'app_bar.dart';
-import 'drawer.dart';
+import '../app_bar.dart';
+import '../drawer.dart';
 
 class NewPoints extends StatefulWidget {
   @override
