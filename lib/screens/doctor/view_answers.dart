e11c322bf20b04d443fc50dbbb8a0fba1a05c3c7
shadwa khaled <shadwakhaled1810621@miuegypt.edu.eg>
2021-12-16 01:23:09 +0200

    viewanswers

--- lib/screens/doctor/view_answers.dart
+++ lib/screens/doctor/view_answers.dart
@@ -1,1 +1,137 @@
 import 'package:flutter/material.dart';
+import '../app_bar.dart';
+import '../drawer.dart';
+
+class ViewAnswers extends StatefulWidget {
+  @override
+  _ViewAnswersState createState() => _ViewAnswersState();
+}
+
+class _ViewAnswersState extends State<ViewAnswers> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer1(),
+        appBar: AppBar(
+          title: App(),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  size: 30,
+                  color: Color(0xff6E85B2),
+                ))
+          ],
+        ),
+        backgroundColor: Color(0xffFFB703),
+        body: Container(
+          decoration: new BoxDecoration(
+              image: new DecorationImage(
+                  image: new AssetImage('assets/view.PNG'), fit: BoxFit.cover)),
+          child: ListView(children: [
+            Column(children: [
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(35),
+                ),
+                child: const ListTile(
+                  title: Text(
+                    "Answers",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Mouse Memoirs',
+                      fontSize: 50,
+                      color: Color(0xffFFFFFF),
+                    ),
+                  ),
+                ),
+                elevation: 30,
+                color: Color(0xff023047),
+                margin:
+                    EdgeInsets.only(bottom: 7, top: 40, left: 90, right: 90),
+              ),
+              Column(
+
+                children: [
+                  Column(
+                      children: [
+                        Padding(padding: EdgeInsets.all(10)),
+                        const Text('Maha: ',
+                            style: TextStyle(
+                                fontSize: 35,
+                                fontFamily: 'Mouse Memoirs',
+                                color: Colors.black)),
+
+                        const Text('Answer1-> A, Answer2-> A, Answer3-> A, Answer4-> A ',
+                            style: TextStyle(
+                                fontSize: 23,
+                                fontFamily: 'Mouse Memoirs',
+                                color: Colors.black))
+                      ]),
+                  Column(
+
+                    children: [
+                    Padding(padding: EdgeInsets.all(10)),
+                    const Text('Salma: ',
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontFamily: 'Mouse Memoirs',
+                            color: Colors.black)),
+                    const Text('Answer1-> B,Answer2-> A, Answer3-> A, Answer4-> A',
+                        style: TextStyle(
+                            fontSize: 23,
+                            fontFamily: 'Mouse Memoirs',
+                            color: Colors.black))
+                  ]),
+                  Column(
+                                         
+children: [
+                    Padding(padding: EdgeInsets.all(10)),
+                    const Text('Shadwa: ',
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontFamily: 'Mouse Memoirs',
+                            color: Colors.black)),
+                    const Text('Answer1-> C,Answer2-> A, Answer3-> A, Answer4-> A',
+                        style: TextStyle(
+                            fontSize: 23,
+                            fontFamily: 'Mouse Memoirs',
+                            color: Colors.black))
+                  ]),
+                  Column(                     
+
+                    children: [
+                    Padding(padding: EdgeInsets.all(10)),
+                    const Text('Bassant: ',
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontFamily: 'Mouse Memoirs',
+                            color: Colors.black)),
+                    const Text('Answer1-> D,Answer2-> A, Answer3-> A, Answer4-> A',
+                        style: TextStyle(
+                            fontSize: 23,
+                            fontFamily: 'Mouse Memoirs',
+                            color: Colors.black))
+                  ]),
+                  Column(
+children: const [
+                    Padding(padding: EdgeInsets.all(10)),
+                    Text('Omar: ',
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontFamily: 'Mouse Memoirs',
+                            color: Colors.black)),
+                    Text('Answer1-> A,Answer2-> A, Answer3-> A, Answer4-> A',
+                        style: TextStyle(
+                            fontSize: 23,
+                            fontFamily: 'Mouse Memoirs',
+                            color: Colors.black))
+                  ]),
+                ],
+              ),
+            ])
+          ]),
+        ));
+  }
+}
