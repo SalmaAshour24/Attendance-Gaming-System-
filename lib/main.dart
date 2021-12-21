e193e908b6e3b389d1a9115e5e17debab4d889d5
shadwa khaled <shadwakhaled1810621@miuegypt.edu.eg>
2021-12-21 20:08:00 +0200

    coursemodell

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter_application_1/screens/doctor/view_courseD.dart';
 import 'package:flutter_application_1/screens/doctor/edit_profile.dart';
 import 'package:flutter_application_1/screens/students/bonus_questions.dart';
 import 'package:flutter_application_1/screens/students/edit_profiles.dart';
-import 'package:flutter_application_1/screens/students/instructions.dart';
 //import 'package:flutter_application_1/screens/students/instructions.dart';
 import 'package:flutter_application_1/screens/students/new_points.dart';
 import 'package:flutter_application_1/screens/students/question_screen.dart';
@@ -46,7 +45,9 @@ class MyApp extends StatelessWidget {
         '/homeD': (context) => ViewCourseD(),
         '/edits': (context) => Editprofiles(),
         '/question': (context) => QuestionScreen(),
-        '/ins': (context) => QuizIns(),
+
+         //'/ins': (context) => QuizIns(),
+
       },
 
       debugShowCheckedModeBanner: false,
@@ -57,7 +58,7 @@ class MyApp extends StatelessWidget {
           "assets/1.PNG",
           fit: BoxFit.fill,
         ),
-        nextScreen: homePage(),
+        nextScreen: HomeScreen(),
         backgroundColor: Color(0xff3D0000),
         splashIconSize: double.infinity,
         splashTransition: SplashTransition.slideTransition,
