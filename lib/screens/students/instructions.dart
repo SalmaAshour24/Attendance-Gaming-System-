9bd8cef83b08a7d4720e43a7fce1ef27994e4d2c
MahaDahshan <mahadahshan@gmail.com>
2022-01-21 03:24:13 +0200

    Update instructions.dart

--- lib/screens/students/instructions.dart
+++ lib/screens/students/instructions.dart
@@ -19,78 +19,87 @@ class _QuizInsState extends State<QuizIns> {
         drawer: Drawer1s(),
         appBar: AppBar(
           title: Apps(),
-          backgroundColor: Color(0xff3D0000),
+          backgroundColor: Color(0xff36344b),
         ),
         backgroundColor: Color(0xffFFB703),
         body: Container(
             decoration: new BoxDecoration(
-                image: new DecorationImage(
-                    image: new AssetImage('assets/4.PNG'), fit: BoxFit.cover)),
+                             color: Color(0xfff8edee),
+            ),
             child: ListView(children: [
               Column(children: [
-                Text(
-                  "Please See the instructions bellow",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Mouse Memoirs',
-                    fontSize: 35,
-                    color: Color(0xff581313),
+                Padding(
+                  padding: const EdgeInsets.only(top:38.0),
+                  child: Text(
+                    "Please See the instructions bellow",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                                  fontFamily: 'Lora-VariableFont_wght',
+                      fontSize: 20,
+                      color: Color(0xff36344b),
+                    ),
                   ),
                 ),
                 SizedBox(
-                  height: 70,
+                  height: 30,
                 ),
-                Container(
-                  height: 400,
-                  color: Color(0xff581313),
-                  child: Column(
-                    children: [
-                      Text(
-                        "1)Feel free to navigate through the questions.",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontFamily: 'Mouse Memoirs',
-                          fontSize: 35,
-                          color: Color(0xffffffff),
-                        ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 400,
+                    color: Color(0xff36344b),
+                    child: Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            "1)Feel free to navigate through the questions.",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: 'Mouse Memoirs',
+                              fontSize: 20,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Text(
+                            "2)You can submit only one time during the exam time.",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: 'Mouse Memoirs',
+                              fontSize: 20,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Text(
+                            "3)The time will start once you started the quiz.",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: 'Mouse Memoirs',
+                              fontSize: 20,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 25,
+                          ),
+                          Text(
+                            "4)If any thing happened during the quiz, Contact your doctor.",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: 'Mouse Memoirs',
+                              fontSize: 20,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Text(
-                        "2)You can submit only one time during the exam time.",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontFamily: 'Mouse Memoirs',
-                          fontSize: 35,
-                          color: Color(0xffffffff),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Text(
-                        "3)The time will start once you started the quiz.",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontFamily: 'Mouse Memoirs',
-                          fontSize: 35,
-                          color: Color(0xffffffff),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 25,
-                      ),
-                      Text(
-                        "4)If any thing happened during the quiz, Contact your doctor.",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          fontFamily: 'Mouse Memoirs',
-                          fontSize: 35,
-                          color: Color(0xffffffff),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -101,7 +110,7 @@ class _QuizInsState extends State<QuizIns> {
                     Navigator.pushNamed(context, '/question');
                   },
                   style: ElevatedButton.styleFrom(
-                    primary: Color(0xff523232),
+                    primary: Color(0xff36344b),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(40),
                     ),
