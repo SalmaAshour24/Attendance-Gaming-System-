e72a66e4a907e1a05079dca3cea6415c9fa02add
MahaDahshan <mahadahshan@gmail.com>
2022-01-23 14:29:48 +0200

    Update drawerS.dart

--- lib/screens/students/drawerS.dart
+++ lib/screens/students/drawerS.dart
@@ -3,7 +3,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_application_1/Model/users_model.dart';
 
 class Drawer1s extends StatefulWidget {
